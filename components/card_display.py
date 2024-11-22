fa67649cca10a2c4f547a0f1aef0e38abefc3953
skainen <karjalainensanteri@gmail.com>
2024-11-22 09:25:28 +0200

    Fixed on_click

--- components/card_display.py
+++ components/card_display.py
@@ -4,21 +4,23 @@ class CardDisplay(ft.UserControl):
     def __init__(self, text: str, on_click):
         super().__init__()
         self.text = text
-        self.on_click = on_click
+        self.on_click_handler = on_click
 
     def build(self):
-        return ft.Card(
-            content=ft.Container(
-                content=ft.Text(
-                    value=self.text,
-                    size=30,
-                    weight=ft.FontWeight.BOLD,
-                    text_align=ft.TextAlign.CENTER
+        return ft.Container(
+            content=ft.Card(
+                content=ft.Container(
+                    content=ft.Text(
+                        value=self.text,
+                        size=30,
+                        weight=ft.FontWeight.BOLD,
+                        text_align=ft.TextAlign.CENTER
+                    ),
+                    padding=20,
+                    alignment=ft.alignment.center,
                 ),
-                padding=20,
-                alignment=ft.alignment.center,
+                width=400,
+                height=200,
             ),
-            width=400,
-            height=200,
-            on_click=self.on_click
+            on_click=self.on_click_handler
         )
